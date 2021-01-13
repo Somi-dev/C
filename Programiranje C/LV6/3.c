9066736bb995ecbf5232fd131e1d8254428f3c81
Somi-dev <53477208+Somi-dev@users.noreply.github.com>
2021-01-13 20:28:52 +0100

    Update 3.c

--- Programiranje C/LV6/3.c
+++ Programiranje C/LV6/3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-   int n, min = 0, max = 0, *p = NULL;
+   int n, min, max, *p = NULL;
 
    printf("Upisite broj clanova polja[4,19>: ");
    while (scanf("%d", &n), n < 4 || n >= 19) {
@@ -13,6 +13,10 @@ int main(void) {
 
    for (int i = 0; i < n; i++) {
       scanf("%d", p + i);
+   }
+
+   min = max = *p;
+   for (int i = 0; i < n; i++) {
       min = min > *(p + i) ? *(p + i) : min;
       max = max < *(p + i) ? *(p + i) : max;
    }
